7d27f5e322ab47dc5dde9536f6b2efd76bb2d497
Anton <anton.benkevich@gmail.com>
2025-05-27 22:34:54 +0100

    фотки в трэд

--- app/main.py
+++ app/main.py
@@ -2,6 +2,26 @@ from app.longpoll import LongPollBot
 from app.summary import summarize_peer, summarize_to_povest
 from app.auth import Auth
 import random
+
+def split_text(summ, max_length=4000):
+    """
+    Разделяет сводку на части, если она превышает максимальную длину.
+    """
+    summary = str(summ)
+    parts = []
+    while len(summary) > max_length:
+        part = summary[:max_length]
+        last_space = part.rfind(' ')
+        if last_space != -1:
+            part = part[:last_space]
+        parts.append(part)
+        summary = summary[len(part):].lstrip()
+    if summary:
+        parts.append(summary)
+    return parts
+
+
+
 if __name__ == "__main__":
     """
     Запуск бота
@@ -20,9 +40,26 @@ if __name__ == "__main__":
     expected_messages = 600  # ожидаемое количество сообщений для сводки
     message_count = 0  # счётчик сообщений
     # summary = summarize_peer(peer_id, last_messages_num=expected_messages, offset=50000)
-    link, summary = summarize_to_povest(peer_id, last_messages_num=expected_messages, offset=random_offset)
+    link, summary, photos = summarize_to_povest(peer_id, last_messages_num=expected_messages, offset=random_offset)    
+    
+    s = split_text(summary)
+    
+    for part in s:
+        vk_api.messages.send(
+            peer_id="2000000066",
+            message=f"{part}",
+            random_id=0
+        )
+    rand_photo = random.sample(photos, min(5, len(photos)))  # случайные 2 фото из списка
+    for photo in rand_photo:
+        vk_api.messages.send(
+            peer_id="2000000066",
+            attachment=photo,
+            message = f"[{photo['url']}|картинка]\n\n{photo['description']}\n\nДата: {photo['date']}\n\n{photo['user_name']}\n\n[{photo['item_link']}|{photo['text'] or 'Оригинал тут'}]",
+            random_id=0
+        )
     vk_api.messages.send(
-    peer_id="2000000066",
-    message=f"{summary}\n\n [{link}|ссылка на начало]",
-    random_id=0
+            peer_id="2000000066",
+            message=f"[{link}|***ссылка на ту часть чатика***]",
+            random_id=0
     )
--- app/summary.py
+++ app/summary.py
@@ -1,6 +1,7 @@
 from app.vk_helpers import VkHelpers
 from app.model import summarize_chat, dramatic_chat_summary, describe_image
-
+import random
+    
 
 # 2000000022
 def summarize_peer(peer_id, last_messages_num, offset=0):
@@ -12,9 +13,11 @@ def summarize_peer(peer_id, last_messages_num, offset=0):
 def summarize_to_povest(peer_id, last_messages_num, offset=0):
     vk = VkHelpers()
     history = vk.get_history_with_names(peer_id, last_messages_num, offset)
-    link, history_txt = history_to_text(history)
-    return link, dramatic_chat_summary(history_txt)
-
+    link = history[0]['link']
+    photos, history_txt = history_to_text(history)
+    text = dramatic_chat_summary(history_txt)
+    return link, text, photos
+    
 def get_attachment_photo_url(attachment):
     if attachment["type"] == "photo":
         photo = attachment["photo"]
@@ -32,22 +35,34 @@ def get_total_photos_in_all_attachments(items):
                 total_photos += 1
     return total_photos
 
+def handle_attachment_photo(attachment, item):
+    photo_url = get_attachment_photo_url(attachment)
+    print(f"Обнаружено фото в сообщении: {item['date']} от {item['user_name']}\n")
+    print(f"Обработка фото: {photo_url}\n")
+    image_describer = describe_image(photo_url)
+    image_description = ""
+    for img_desc in image_describer.stream({}, stream_mode=['messages']):
+        print(img_desc.content, end='', flush=True)
+        image_description += img_desc.content
+    return photo_url, image_description
+
 def history_to_text(history):
-    link = history[0]['link']
     text = ""
     print(f"Всего фото в этом куске: {get_total_photos_in_all_attachments(history)}\n")                    
+    photos = []
     for item in history:
         if item['attachments']:
             for attachment in item['attachments']:
                 if attachment['type'] == 'photo':
-                    photo_url = get_attachment_photo_url(attachment)
-                    print(f"Обнаружено фото в сообщении: {item['date']} от {item['user_name']}\n")
-                    print(f"Обработка фото: {photo_url}\n")
-                    image_describer = describe_image(photo_url)
-                    image_description = ""
-                    for img_desc in image_describer.stream({}, stream_mode=['messages']):
-                        print(img_desc.content, end='', flush=True)
-                        image_description += img_desc.content
+                    photo_url, image_description = handle_attachment_photo(attachment, item)
+                    photos.append({
+                        'url': photo_url,
+                        'description': image_description,
+                        'date': item['date'],
+                        'user_name': item['user_name'],
+                        'text': item['text'],
+                        'item_link': item['link']
+                    })
                     text += f"({item['date']}) {item['user_name']} добавил фото на котором изображено: ***НАЧАЛО ФОТО*** \n\n{image_description}\n\n ***КОНЕЦ ФОТО***"
         text += f"({item['date']}) {item['user_name']}: {item['text']}\n\n"
-    return link, text
+    return photos, text
